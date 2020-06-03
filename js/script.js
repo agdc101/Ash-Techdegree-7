a8356bf379fd0f280cfcc14b9a787c91e68b8293
Ashley Campbell <agdc101@gmail.com>
2020-06-03 18:15:42 +0100

    Ready for Review!

--- js/script.js
+++ js/script.js
@@ -1,53 +1,5 @@
-/*--------- Alert Div function--------------*/
-const alertDiv = document.querySelector('#alert');
-alertDiv.innerHTML = 
-                    `<p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
-                    <button class="alert-close">x</button>`;
-
-alertDiv.addEventListener('click', e => { 
-    if (e.target.tagName === 'BUTTON') {
-        alertDiv.style.transform = 'scale(0)';
-        setTimeout( function () { 
-            alertDiv.style.display = 'none'; 
-        }, 700);
-    }
-});
-/*------- Notification Div function---------*/
-const notiDiv = document.querySelector('#notifications');
-const notiButton = document.querySelector('.notifications');
-const dot = document.querySelector('.dot');
-notiDiv.style.display = 'none';
-notiDiv.innerHTML = `<div class='alert_flex'><p>You have 6 unread messages</p> <button>x</button></div>
-                     <div class='alert_flex'><p>You have 3 new followers</p> <button>x</button></div>
-                     <div class='alert_flex'><p>Your password expires in 7 days</p> <button>x</button></div>`;
-const notiItems = document.querySelectorAll('.alert_flex');
-
-notiButton.addEventListener('click', () => {
-    dot.style.display = 'none';
-    if ( notiDiv.style.display === 'none' && notiItems.length > 0) {
-        notiDiv.style.display = 'block';
-    } else {
-        notiDiv.style.display = 'none'
-    }
-    
-});
-notiDiv.addEventListener('click', (e) => {
-    
-    if ( e.target.tagName === 'BUTTON') {
-    e.target.parentNode.remove();
-    console.log(notiItems.length);
-        if ( notiItems.length === 1 ) {
-            notiDiv.style.display = 'none';
-        }
-    }
-
-});
-
-/*--------- ----------- --------------------*/
-/*---------------- CHART.JS-----------------*/
 
 /*----------LINE GRAPH VARIABLES---- -------*/
-
 const lineGraphX = document.querySelector('#lineGraph');
 const dataHourly = [0, 150, 850, 400, 800, 500, 1050, 550, 1250, 650, 1150, 2000];
 const dataDaily = [0, 500, 150, 450, 1050, 1200, 1750, 1050, 1650, 1450, 1500, 2500];
@@ -60,6 +12,74 @@ const monthly = document.querySelector('li:nth-child(4)');
 const listButtons = document.querySelectorAll('.traffic_list li button');
 const trafficList = document.querySelector('.traffic_list');
 /*--------- ----------- --------------------*/
+/*----------BAR CHART VARIABLES---- --------*/
+const barChartX = document.querySelector('#barChart');
+/*--------- ----------- --------------------*/
+/*----------PIE CHART VARIABLES---- --------*/
+const pieChartX = document.querySelector('#pieChart');
+/*--------- ----------- --------------------*/
+/*--------NOTIFICATION DIV VARIABLES---- ---*/
+const notiDiv = document.querySelector('#notifications');
+const notiButton = document.querySelector('.notifications');
+const dot = document.querySelector('.dot');
+/*--------- ----------- --------------------*/
+/*----------ALERT DIV VARIABLES---- --------*/
+const alertDiv = document.querySelector('#alert');
+/*--------- ----------- --------------------*/
+/*---------MESSAGE SECTION VARIABLES--------*/
+const userSearch = document.querySelector('#user_search');
+const userMessage = document.querySelector('#user_message');
+const userSend = document.querySelector('#msg_send_button');
+/*--------- ----------- --------------------*/
+/*----------LOCAL STORAGE VARIABLES---------*/
+const emailSwitch  = document.querySelector('#email_switch');
+const profileSwitch  = document.querySelector('#profile_switch');
+const buttonsDiv = document.querySelector('.form_buttons');
+const timezone = document.querySelector('#timezone');
+/*--------- ----------- --------------------*/
+
+/*---------------JS FUNCTIONS---------------*/
+/*--------- Alert Div function--------------*/
+alertDiv.innerHTML = 
+                    `<p><strong>Alert:</strong> You have <strong>6</strong> overdue tasks to complete</p>
+                    <button class="alert-close">x</button>`;
+
+alertDiv.addEventListener('click', e => { 
+    if (e.target.tagName === 'BUTTON') {
+        alertDiv.style.transform = 'scale(0)';
+        setTimeout( function () { 
+            alertDiv.style.display = 'none'; 
+        }, 700);
+    }
+});
+
+/*------- Notification Div function---------*/
+notiDiv.style.display = 'none';
+notiDiv.innerHTML = `<div class='alert_flex'><p>You have 6 unread messages</p> <button>x</button></div>
+                     <div class='alert_flex'><p>You have 3 new followers</p> <button>x</button></div>
+                     <div class='alert_flex'><p>Your password expires in 7 days</p> <button>x</button></div>`;
+const notiItems = document.querySelectorAll('.alert_flex');
+
+notiButton.addEventListener('click', () => {
+    dot.style.display = 'none';
+    if ( notiDiv.style.display === 'none' && notiItems.length > 0) {
+        notiDiv.style.display = 'block';
+    } else {
+        notiDiv.style.display = 'none'
+    }    
+});
+notiDiv.addEventListener('click', (e) => {
+    
+    if ( e.target.tagName === 'BUTTON') {
+    e.target.parentNode.remove();
+    console.log(notiItems.length);
+        if ( notiItems.length === 1 ) {
+            notiDiv.style.display = 'none';
+        }
+    }
+});
+/*--------- ----------- --------------------*/
+/*-------------- CHART.JS-------------------*/
 /*--------------LINE GRAPH--- --------------*/
 let lineGraph = new Chart(lineGraphX, {
     type: 'line',
@@ -98,12 +118,12 @@ let lineGraph = new Chart(lineGraphX, {
         }       
 });
 /*--------- ----------- --------------------*/
-/*-- -----LINE GRAPH DATA FUNCTION--------*/
+/*-- ------LINE GRAPH DATA FUNCTION---------*/
+// This function accepts the new data as the parameter then updates the graph!
 const lineGraphChange = (data) => {
     lineGraph.data.datasets[0].data = data;
     lineGraph.update();      
 }
-
 // --- LINE GRAPH CLICK HANDLER
 trafficList.addEventListener('click', (e) => {
     if (e.target.tagName === 'BUTTON') {
@@ -125,8 +145,6 @@ trafficList.addEventListener('click', (e) => {
 
 /*--------- ----------- --------------------*/
 /*------- -------BAR CHART -----------------*/
-const barChartX = document.querySelector('#barChart');
-
 let barChart = new Chart(barChartX, {
     type: 'bar',
     data: {
@@ -154,8 +172,6 @@ let barChart = new Chart(barChartX, {
 });
 /*--------- ----------- --------------------*/
 /*------- -------PIE CHART -----------------*/
-const pieChartX = document.querySelector('#pieChart');
-
 let pieChart = new Chart(pieChartX, {
     type: 'doughnut',
     data: {
@@ -169,8 +185,7 @@ let pieChart = new Chart(pieChartX, {
             '#78CF82',
             '#51B6C8'
             ]
-    }]
-                                                                                
+    }]                                                                                
     },
     options: {
         responsive: true,
@@ -185,10 +200,7 @@ let pieChart = new Chart(pieChartX, {
 });
 /*--------- ----------- --------------------*/
 /*------- ----MESSAGE SECTION JS -----------*/
-const userSearch = document.querySelector('#user_search');
-const userMessage = document.querySelector('#user_message');
-const userSend = document.querySelector('#msg_send_button');
-
+// This ensures the user has entered info into all the fields before they 'clicl' send button.
 userSend.addEventListener('click', () => {
     if ( userSearch.value === '' && userMessage.value === '' ) {
         alert('Search and message fields are both blank!');
@@ -199,5 +211,38 @@ userSend.addEventListener('click', () => {
     } else {
         alert(`Thanks! Your message was sent successfully sent to ${userSearch.value}`);
     }
-})
+});
 /*--------- ----------- --------------------*/
+/*---- ----LOCAL STORAGE function ----------*/
+buttonsDiv.addEventListener('click', (e) => {
+    if (e.target.textContent === 'SAVE') {
+        if (timezone.value === 'Select a Timezone') {
+            // We don't
+            alert('Please select a Timezone!');
+        } else {
+            localStorage.setItem('emailSwitch', emailSwitch.checked);
+            localStorage.setItem('profileSwitch', profileSwitch.checked);
+            localStorage.setItem('timezone', timezone.value);
+        }
+    }   
+    if (e.target.textContent === 'CANCEL') {
+        localStorage.clear();
+        location.reload();
+        }
+});
+window.addEventListener('load', () => {
+    if (localStorage.getItem("emailSwitch") !== null) {
+        const emailChecked = JSON.parse(localStorage.getItem('emailSwitch'));
+        const profileChecked = JSON.parse(localStorage.getItem('profileSwitch'));
+        const timezoneValue = localStorage.getItem('timezone');
+        if (emailChecked === true && profileChecked === true) {
+            emailSwitch.checked = true;
+            profileSwitch.checked = true;
+        } else if (profileChecked === true) {
+            profileSwitch.checked = true;
+        } else {
+            emailSwitch.checked = true;
+        }
+        timezone.value = timezoneValue;
+    }
+})
